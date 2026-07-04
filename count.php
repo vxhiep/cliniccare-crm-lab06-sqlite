ea18d81436a3329a8e7a05d2c269ef0aabe80048
vxhiep <vuh266882@gmail.com>
2026-07-04 21:06:04 +0700

    docs: update readme and report

---
+++ count.php
@@ -0,0 +1,5 @@
+﻿<?php
+$db = new PDO('sqlite:database/cliniccare.sqlite');
+
+echo 'patients=' . $db->query('SELECT COUNT(*) FROM patients')->fetchColumn() . PHP_EOL;
+echo 'appointments=' . $db->query('SELECT COUNT(*) FROM appointments')->fetchColumn() . PHP_EOL;
